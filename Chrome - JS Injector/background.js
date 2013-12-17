8327e2ffac381d1c629b9fb3c48e1b8befc75f4b
Chris West <cwest@monetate.com>
2013-12-17 16:57:13 -0500

    Version 1.0

---
+++ Chrome - JS Injector/background.js
@@ -0,0 +1,212 @@
+chrome.webNavigation.onDOMContentLoaded.addListener(function(info) {
+    if (/^http/i.test(info.url) && !info.frameId) {
+        var code = '(function(s){;s.innerHTML=unescape("'
+            + escape(
+                '(' + injectCode + ')(' +
+                (localStorage.getItem('scripts') || '[]') +
+                ')'
+            )
+            + '");document.head.appendChild(s);})(document.createElement("script"))';
+        chrome.tabs.executeScript(info.tabId, { code: code });
+    }
+});
+
+var inject = function(scripts) {
+    var hasOwnProperty = ({}).hasOwnProperty;
+
+    var cookies = {};
+    document.cookie.split(/;\s+/).map(function(cookie) {
+        if (cookie = /^([^=]+)=(.*)$/.exec(cookie)) {
+            cookies[unescape(cookie[1])] = unescape(cookie[2]);
+        }
+    });
+
+    var location = {
+        hostname: window.location.hostname,
+        pathname: window.location.pathname,
+        protocol: window.location.protocol,
+        origin: window.location.origin,
+        search: window.location.search,
+        hash: window.location.hash,
+        host: window.location.host,
+        href: window.location.href,
+        port: window.location.port,
+        param: urlParam
+    };
+
+    var urlParams = {};
+    location.search.replace(
+        /(?:^|)[\?&]([^=]*)(?:=([^&]*))/g,
+        function(match, key, value) {
+            urlParams[unescape(key.replace(/\+/g, ' '))] = unescape(value.replace(/\+/g, ' '));
+        }
+    );
+
+    var RGX_VARIABLE = /(?:(?!^)\.)?([\$_A-Za-z][\$\w]*)|\[((["'])(?:[^\\]+|\\.|(?!\1)["'])\1|\d+)\]/g;
+    function varExists(name, opt_typeName) {
+        var varToCheck = window;
+        var varFound = true;
+        var remainder = name.replace(
+            RGX_VARIABLE,
+            function(match, varName, strVarName, strDelim, index, original) {
+                if (!varFound) {
+                    throw new Error(
+                        'There is no variable named "' +
+                        original.slice(0, index) + '".'
+                    );
+                }
+                varName = varName || eval(strVarName);
+                if (varFound = (hasOwnProperty.call(varToCheck, varName))) {
+                    varToCheck = varToCheck[varName];
+                }
+                return '';
+            }
+        );
+        if (remainder) {
+            throw new Error(
+                'Unexpected character in variable "' + name + '".'
+            );
+        }
+        return opt_typeName ? typeOf(varFound, opt_typeName) : varFound;
+    }
+
+    var arrSlice = [].slice;
+
+    function curry(fn) {
+        var firstArgs = arrSlice.call(arguments, 1);
+        return function() {
+            return fn.apply(this, firstArgs.concat(arrSlice.call(arguments, 0)));
+        };
+    }
+
+    var typeOf = (function() {
+        var toString = ({}).toString;
+        return function(o, t) {
+            o = toString.call(o).slice(8, -1);
+            return t ? t == o : o;
+        };
+    })();
+
+    function cookie(name, value) {
+        if (hasOwnProperty.call(cookies, name)) {
+            var cookieValue = cookies[name];
+            return arguments.length > 1
+                ? typeOf(value, 'RegExp')
+                    ? value.test(cookieValue)
+                    : cookieValue == value
+                : cookieValue;
+        }
+    }
+
+    function urlParam(name, value) {
+        if (hasOwnProperty.call(urlParams, name)) {
+            var urlValue = urlParams[name];
+            return arguments.length > 1
+                ? equals(urlValue, value)
+                : urlValue;
+        }
+    }
+
+    function startsWith(str, substring, ignoreCase) {
+        if (ignoreCase) {
+            str = str.toUpperCase();
+            substring = substring.toUpperCase();
+        }
+        return str.slice(0, substring.length) == substring;
+    }
+
+    function endsWith(str, substring, ignoreCase) {
+        if (ignoreCase) {
+            str = str.toUpperCase();
+            substring = substring.toUpperCase();
+        }
+        return str.slice(-substring.length) == substring;
+    }
+
+    function contains(str, substring, ignoreCase) {
+        if (ignoreCase) {
+            str = str.toUpperCase();
+            substring = substring.toUpperCase();
+        }
+        return str.indexOf(substring) > -1;
+    }
+
+    function equals(str, value) {
+        return typeOf(str, 'String') && typeOf(value, 'RegExp')
+            ? value.test(str)
+            : str === value;
+    }
+
+    function elements(selector, attr, value) {
+        var isValueGiven = arguments.length > 2;
+        var isAttrGiven = isValueGiven || arguments.length > 1;
+        var elems = document.querySelectorAll(selector);
+        var ret = [];
+        for (var i = 0, len = elems.length; i < len; i++) {
+            var elem = elems[i];
+            if (!isAttrGiven || (
+                    isValueGiven
+                        ? equals(elem.getAttribute(attr) || elem[attr], value)
+                        : (elem.hasAttribute(attr) || (attr in elem)))
+                    ) {
+
+                ret.push(elem);
+            }
+        }
+        return ret;
+    }
+
+    function executeScripts(eventName) {
+        // Execute the filters and if they pass the scripts.
+        console.group('JS Injector (' + eventName + ')');
+        var startTime = new Date;
+        scripts.forEach(function(script) {
+            if ((script.loadTime || 'onLoad') == eventName) {
+                var useScript;
+                try {
+                    var filter = new Function('elements', 'location', 'cookie',
+                        'urlParam', 'startsWith', 'endsWith', 'contains',
+                        'typeOf', 'varExists', 'equals',
+                        'return ' + (script.filter.trim() || 'true'));
+                    useScript = filter(elements, location, cookie, urlParam,
+                        startsWith, endsWith, contains, typeOf, varExists,
+                        equals);
+                }
+                catch (e) {
+                    console.group(script.name);
+                    console.log('Filter error:', e.message);
+                    console.groupEnd();
+                }
+
+                if (useScript) {
+                    console.group(script.name);
+                    try {
+                        eval(script.code);
+                    }
+                    catch (e) {
+                        console.log('Code error:', JSON.stringify(e, null, 1));
+                    }
+                    console.groupEnd();
+                }
+            }
+        });
+        var endTime = new Date;
+        console.log('Start time:  ', startTime);
+        console.log('Elapsed time (milliseconds):  ', endTime - startTime);
+        console.log('End time:  ', endTime);
+        console.groupEnd();
+    }
+
+    // Execute the scripts to execute on DOMContentLoaded.
+    executeScripts('onDOMContentLoaded');
+
+    // Once the page and all resources have loaded, run the onload scripts.
+    var onload = curry(executeScripts, 'onLoad');
+    if (document.readyState == 'complete') {
+        onload();
+    } else {
+        window.addEventListener('load', onload);
+    }
+};
+
+var injectCode = inject.toString();
---
+++ Chrome - JS Injector/options.js
@@ -0,0 +1,397 @@
+$(function() {
+  var jWindow = $(window);
+  var jBody = $('body');
+  var jDialog = $('#dialog');
+  var jDomLoaded = $('#radDomLoaded');
+  var jLoad = $('#radLoad');
+  var jTabs = $('#tabs');
+  var jTab1 = jTabs.find('a[href$="#tab1"]');
+  var jTab2 = jTabs.find('a[href$="#tab2"]');
+  var jTab3 = jTabs.find('a[href$="#tab3"]');
+  var jSortable = $('#sortable');
+  var jFilter = $('#filter');
+  var jCode = $('#code');
+  var jName = $('#txtName');
+  var aceFilter, aceCode;
+  var scripts = JSON.parse(localStorage.getItem('scripts') || '[]');
+  var scriptIndex;
+  var script;
+
+  function saveScript() {
+    if (validate()) {
+      saveAll();
+      $(this).dialog('close');
+    }
+  }
+  function cancelScript() {
+    $(this).dialog('close');
+  }
+  function deleteScript() {
+    if (confirm('Are you sure you want to delete this script permanently?')) {
+      // The list item for this script.
+      var jLI = $('li[data-index="' + scriptIndex + '"]');
+
+      // Decreases the data-index of each LI after the one that is deleted.
+      jLI.nextAll('li')
+        .each(function() {
+          var jLI = $(this);
+          jLI.attr('data-index', +jLI.attr('data-index') - 1);
+        });
+
+      // Remove the list item for this script.
+      jLI.remove();
+
+      // Remove the script from the scripts array.
+      scripts.splice(scriptIndex, 1);
+
+      // Commit the changes to local storage.
+      saveAll();
+    }
+    $(this).dialog('close');
+  }
+
+  var DIALOG_BUTTONS_NEW = {
+    'Add Script': saveScript,
+    'Cancel': cancelScript
+  };
+  var DIALOG_BUTTONS_MODIFY = {
+    'Update Script': saveScript,
+    'Delete Script': deleteScript,
+    'Cancel': cancelScript
+  };
+
+  /**
+   * Validates tabbed data and shows any applicable.  If no errors were found,
+   * the data is saved in the scripts array.
+   * @return {boolean}  True if no errors occurred, otherwise false.
+   */
+  function validate() {
+    var errors = [];
+
+    // Check the name.
+    var name = jName.val();
+    if (name) {
+      for (var i = 0, len = scripts.length; i < len; i++) {
+        var thatScript = scripts[i];
+        if (thatScript != script
+            && script.name.toUpperCase() == thatScript.name.toUpperCase()) {
+
+          errors.push({
+            msg: 'There is another script with this name.',
+            tab: jTab1
+          });
+        }
+      }
+    }
+    else {
+      errors.push({
+        msg: 'You must enter a name for the script.',
+        tab: jTab1
+      });
+    }
+
+    // Check the filter.
+    var filter = aceFilter.getValue();
+    try {
+      checkFilter(filter);
+    }
+    catch (e) {
+      errors.push({
+        msg: 'Filter - ' + e.message,
+        tab: jTab2
+      });
+    }
+
+    // Check the code.
+    var code = aceCode.getValue().trim();
+    if (code) {
+      try {
+        new Function(code);
+      }
+      catch (e) {
+        errors.push({
+          msg: 'JS Code - ' + e.message,
+          tab: jTab3
+        });
+      }
+    }
+    else {
+      errors.push({
+        msg: 'You must add JavaScript code.',
+        tab: jTab3
+      });
+    }
+
+    // Show any errors that were found (clicking the tab for the first one).
+    var error1 = errors[0];
+    if (error1) {
+      alert(
+        'The following issues must be resolved befores saving:' +
+        errors.map(function(error) { return '\n- ' + error.msg; }).join('')
+      );
+      error1.tab.click();
+    }
+    // If no errors occurred, go ahead and move the script to the scripts array.
+    // Also make necessary changes to the DOM.
+    else {
+      script.name = name;
+      script.loadTime = $('input:radio[name="loadTime"]:checked').val();
+      script.filter = filter;
+      script.code = code;
+
+      // If adding this script, add it to the scripts array and the DOM.
+      if (scriptIndex < 0) {
+        scriptIndex = $('#sortable > li').index(jLineItemAddScript);
+
+        // Insert the script object into the scripts array.
+        scripts.splice(scriptIndex, 0, script);
+
+        // Insert new LI in the DOM.
+        var jLineItemNew = jLineItem
+          .clone(true)
+          .attr('data-index', scriptIndex)
+          .insertAfter(jLineItemAddScript);
+        jLineItemNew.find('a').text(name);
+
+        // Increases the data-index of each LI after the one that is inserted.
+        jLineItemNew.nextAll('li')
+          .each(function() {
+            var jLI = $(this);
+            jLI.attr('data-index', +jLI.attr('data-index') + 1);
+          });
+      }
+      // If updating make sure to change the display name of the script.
+      else {
+        $('#sortable > li[data-index="' + scriptIndex + '"] > a').text(name);
+      }
+    }
+
+    // Return a boolean indicating if no errors occurred.
+    return !error1;
+  }
+
+  function checkFilter(filter) {
+    var ALLOWED_WORDS = [
+      '.length',
+      '.toLowerCase()',
+      '.toUpperCase()',
+      '.search',
+      '.test',
+      'startsWith',
+      'endsWith',
+      'contains',
+      'equals',
+      'cookie',
+      'elements',
+      'typeOf',
+      'varExists',
+      'urlParam',
+      'location.hash',
+      'location.host',
+      'location.hostname',
+      'location.href',
+      'location.origin',
+      'location.param',
+      'location.pathname',
+      'location.port',
+      'location.protocol',
+      'location.search',
+      'undefined',
+      'null',
+      'true',
+      'false',
+      'new',
+      'Date'
+    ];
+    var nextIndex = 0;
+    var rgxToken = /\s*(?:([\!\=\<\>]\=|[\!\(\)\<\>\+\,]|\&\&|\|\|)|((['"])(?:\\.|(?!\3|\\).)*\3)|(\/(?:[^\\\/]|\\.)+\/[gim]*)|((?:[A-Za-z]*\.)?[A-Za-z]+)|(-?\d+))/g;
+    var remains = filter.replace(
+      rgxToken,
+      function(match, operator, string, stringDelim, rgx, variable, number, index, original) {
+        // If an unexpected group of characters was encountered, throw an error.
+        if (nextIndex < index) {
+          var c = getPos(original, /\S/, nextIndex);
+          throw new Error(
+            'Unexpected character on line ' + c.line + ', character ' +
+            (c.index + 1) + '.'
+          );
+        }
+
+        // If the variable isn't one of the ones that is allowed, throw an error.
+        if (variable && ALLOWED_WORDS.indexOf(variable) < 0) {
+          var c = getPos(original, '', index);
+          throw new Error(
+            'Unexpected "' + variable + '" variable on line ' + c.line +
+            ', character ' + (c.index + 1) + '.'
+          );
+        }
+
+        // Set the next expected starting point.
+        nextIndex = index + match.length;
+
+        // Done to do a final check as to if any characters were left
+        // unaccounted for.
+        return '';
+      }
+    );
+    
+    if (remains.trim()) {
+      var c = getPos(filter, /\S/, filter.length - remains.length);
+      throw new Error(
+        'Unexpected character on line ' + c.line + ', character ' +
+        (c.index + 1) + '.'
+      );
+    }
+
+    // Try to actually compile the code.
+    new Function('return ' + filter);
+
+    // Return the filter passed in.
+    return filter;
+  }
+
+  function getPos(str, target, charOffset) {
+    var remains = str.slice(charOffset || 0);
+    charOffset = remains[typeOf(target, 'RegExp') ? 'search' : 'indexOf'](target);
+    if (charOffset >= 0) {
+      remains = str.slice(0, str.length - remains.length + charOffset).split(/\r?\n|\r/);
+      return {
+        line: remains.length,
+        index: remains[remains.length - 1].length
+      };
+    }
+  }
+
+  var typeOf = (function() {
+    var toString = ({}).toString;
+    return function(o, t) {
+      o = toString.call(o).slice(8, -1);
+      return t ? t == o : o;
+    };
+  })();
+
+  function saveAll() {
+    localStorage.setItem('scripts', JSON.stringify(scripts));
+  }
+
+  // Changes the order of the scripts array after the user resorts.
+  jSortable.sortable({
+    stop: function( event, ui ) {
+      var newScripts = [];
+      var i = 0;
+      $('#sortable > li').each(function() {
+        var jMe = $(this);
+        var index = +jMe.attr('data-index');
+        if (index > -1) {
+          jMe.attr('data-index', i++);
+          newScripts.push(scripts[index]);
+        }
+      });
+      scripts = newScripts;
+      saveAll();
+    }
+  });
+  jSortable.disableSelection();
+
+  // Create a normal sortable script line item.
+  var jLineItem = $(
+    '<li class="ui-state-default">' +
+      '<span class="ui-icon ui-icon-arrowthick-2-n-s"></span>' +
+      '<a class="script-title"></a>' +
+    '</li>'
+  );
+  $('a', jLineItem).click(function() {
+    var jMe = $(this);
+    var jLineItem = jMe.parent();
+    scriptIndex = +jLineItem.attr('data-index');
+    script = scripts[scriptIndex] || {
+      name: '',
+      filter: '',
+      code: ''
+    };
+    jDialog
+      .dialog('option', {
+        title: script.name || '[Unnamed Script]',
+        buttons: scriptIndex < 0 ? DIALOG_BUTTONS_NEW : DIALOG_BUTTONS_MODIFY
+      })
+      .dialog('open');
+  });
+
+  // "ADD A SCRIPT" line item.
+  var jLineItemAddScript = jLineItem.clone(true).attr('data-index', '-1');
+  jLineItemAddScript.find('a')
+    .prop('id', 'addScriptTitle')
+    .text('>>> ADD A SCRIPT <<<');
+  jSortable.append(jLineItemAddScript);
+
+  // Initialize the dialog box.
+  jDialog.dialog({
+    autoOpen: false,
+    closeOnEscape: false,
+    modal: true,
+    draggable: false,
+    resizable: false,
+    open: function() {
+      jTab1.click();
+      jName.val(script.name);
+      var loadTime = script.loadTime || 'onLoad';
+      jDomLoaded[0].checked = loadTime == 'onDOMContentLoaded';
+      jLoad[0].checked = loadTime == 'onLoad';
+      aceFilter.setValue(script.filter);
+      aceCode.setValue(script.code);
+      jWindow.resize();
+    }
+  });
+
+  // Setup the jQuery tabs.
+  jTabs.tabs();
+  jTab1.click(function() {
+    jName.focus();
+  });
+  jTab2.click(function() {
+    aceFilter.focus();
+    aceFilter.gotoLine(1, 0);
+  });
+  jTab3.click(function() {
+    aceCode.focus();
+    aceCode.gotoLine(1, 0);
+  });
+
+  jName.change(function() {
+    jDialog.dialog('option', 'title', this.value || '[Unnamed Script]');
+  });
+
+  // Setup elements so that on a resize everything will still look good.
+  jWindow.resize(function() {
+    jDialog.dialog(
+      'option',
+      {
+        height: jWindow.height() - 100,
+        width: jWindow.width() - 100
+      }
+    );
+
+    var height = jTabs.parent().height() - 10;
+    jTabs.height(height);
+    $([jFilter[0], jCode[0]]).height(height - 55);
+  });
+
+  // Show the saved scripts.
+  scripts.forEach(function(script, index) {
+    jLineItem.clone(true)
+      .appendTo(jSortable)
+      .attr('data-index', index)
+      .find('a')
+        .text(script.name);
+  });
+
+  // Setup ACE (http://ace.c9.io/).
+  var aceFilter = ace.edit("filter");
+  aceFilter.setTheme("ace/theme/clouds");
+  aceFilter.getSession().setMode("ace/mode/javascript");
+
+  // Setup ACE (http://ace.c9.io/).
+  var aceCode = ace.edit("code");
+  aceCode.setTheme("ace/theme/clouds");
+  aceCode.getSession().setMode("ace/mode/javascript");
+});
